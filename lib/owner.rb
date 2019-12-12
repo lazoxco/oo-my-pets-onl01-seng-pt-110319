4410bc585c9de98914759b1792a648c8295a0f58
Nory Lazo <nlazo@icloud.com>
2019-12-12 08:01:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -54,6 +54,12 @@ class Owner
   end 
   
   def sell_pets 
+    cats.each do |cat|
+      cat.mood = "nervous"
+    end 
     
+    dogs.each do |dog|
+      dog.mood = "nervous"
+    end 
   end 
 end
